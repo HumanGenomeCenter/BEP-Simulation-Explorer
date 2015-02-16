ae5c7095348f1c59a2a5c91a8b45b25442896240
trembl <g@trembl.org>
2015-02-16 17:46:04 +0900

    404 Not Found

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php include("header.php"); ?>
+
+<div class="row">
+	
+	<div class="col-xs-12">
+		<p class="lead">
+			404 - Page Not Found.
+		</p>
+	</div>
+	
+</div>
+
+<?php include("footer.php"); ?>
