3d438c8b286c90529a9c27ec4415a087b511c524
trembl <g@trembl.org>
2015-02-18 14:54:12 +0900

    Gradient

--- distribution.php
+++ distribution.php
@@ -2,9 +2,10 @@
 
 <style type="text/css" media="screen">
 
-.area{
+.area, .color {
   shape-rendering: geometricPrecision;
-  fill: #ccc !important;
+/*  fill: #f00 !important; */
+  fill: url(#grad);
 }
 .boxplot{
   shape-rendering: crispEdges;
@@ -31,132 +32,122 @@
 
 .axis path, .axis line {
   fill: none;
-  stroke: #000;
+  stroke: #999;
   stroke-width: 1px;
   color-rendering: optimizeQuality !important;
   shape-rendering: crispEdges !important;
   text-rendering: geometricPrecision !important; 
 
 }
+svg {
+	background: #eee;
+}
+
+
 </style>
 
 
 <div class="row">
 	
 	<div class="col-xs-12">
-		<div id="svgElement1"></div>
-		<div id="svgElement2"></div>
+		<div id="distribution"></div>
+	
 		
 	</div>
 	
 </div>
 
 <script src="js/d3.min.js" type="text/javascript" charset="utf-8"></script>
+
 <script type="text/javascript">
-
-// random distribution
-
-var results = [];
-
-for (var i = 0; i < 2; i++) {
-	var data = [];
-	for (var i = 0; i < 1000; i++) {
-		data.push((Math.random()*10)-5);
-	}
-	results.push(data);
-}
-
-function addViolin(svg, results, height, width, domain, imposeMax, violinColor){
+	
+// Generate a Bates distribution of 10 random variables.
+var max = 10;
+var values = d3.range(100).map(d3.random.irwinHall(max));
 
 
-	var data = d3.layout.histogram()
-		.bins(50)
-		.frequency(0);
-        (results);
+// A formatter for counts.
+var formatCount = d3.format(",.0f");
 
-        var y = d3.scale.linear()
-                    .range([width/2, 0])
-                    .domain([0, Math.max(imposeMax, d3.max(data, function(d) { return d.y; }))]);
+var margin = {top: 30, right: 30, bottom: 60, left: 30},
+    width = 200,
+    height = 100;
 
-        var x = d3.scale.linear()
-                    .range([height, 0])
-                    .domain(domain)
-                   // .nice();
+var x = d3.scale.linear()
+	.domain([0, max])
+	.range([0, width]);
 
+// Generate a histogram using twenty uniformly-spaced bins.
+var data = d3.layout.histogram()
+    .bins(x.ticks(20))
+    (values);
 
-        var area = d3.svg.area()
-            .interpolate(interpolation)
-            .x(function(d) {return x(d.x);})
-            .y0(width-20)
-            .y1(function(d) { return y(d.y*2); });
-
-    
-        var gMinus=svg.append("g")
-
-        gMinus.append("path")
-          .datum(data)
-          .attr("class", "area")
-          .attr("d", area)
-          .style("fill", violinColor);
-
-
-
-}
-
-var margin={top:10, bottom:30, left:30, right:10};
-
-var width=600;
-var height=200;
-var boxWidth=100;
-var boxSpacing=10;
-var domain=[-10,10];
-
-var resolution=20;
-var d3ObjId="svgElement1";
-var interpolation='step-before';
 
 var y = d3.scale.linear()
-            .range([height-margin.bottom, margin.top])
-            .domain(domain);
+    .domain([0, d3.max(data, function(d) { return d.y; })])
+    .range([height, 0]);
 
-var yAxis = d3.svg.axis()
-                .scale(y)
-                .ticks(5)
-                .orient("left")
-                .tickSize(5,0,5);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
+var svg = d3.select("#distribution").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
 
-var d3ObjId="svgElement2";
-var interpolation='basis';
-
-var svg = d3.select("#"+d3ObjId)
-            .append("svg")
-                .attr("width", width)
-                .attr("height", height);
-
-svg.append("line")
-    .attr("class", "boxplot")
-    .attr("x1", margin.left)
-    .attr("x2", width-margin.right)
-    .attr("y1", y(0))
-    .attr("y2", y(0));
-
-for(var i=0; i<results.length; i++){
-    results[i]=results[i].sort(d3.ascending)
-    var g=svg.append("g").attr("transform", "translate("+(i*(boxWidth+boxSpacing)+margin.left)+",0)");
-    addViolin(g, results[i], height, boxWidth, domain, 0.25, "#cccccc");
- //   addBoxPlot(g, results[i], height, boxWidth, domain, .15, "black", "white");
-
-}
-
-/*
-svg.append("g")
-    .attr('class', 'axis')
-    .attr("transform", "translate("+margin.left+",0)")
-    .call(yAxis);
-*/
-
+// definition for gradient
+var defs = svg.append("defs");
 	
+defs.append("linearGradient")
+     .attr("id", "grad")
+ //    .attr("gradientUnits", "userSpaceOnUse")
+     .attr("x1", 0).attr("y1", 0)
+     .attr("x2", 1).attr("y2", 0)
+   .selectAll("stop")
+     .data([
+		 {offset: "20%", color: "white"},
+		{offset: "80%", color: "red"}
+     ])
+   .enter().append("stop")
+     .attr("offset", function(d) { return d.offset; })
+     .attr("stop-color", function(d) { return d.color; });
+	 
+svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+
+var interpolation = "basis";
+
+var area = d3.svg.area()
+	.interpolate("basis")
+	.x(function(d) { return x(d.x+d.dx/2); })
+	.y0(height)
+	.y1(function(d) { return y(d.y); });
+	
+var g = svg.append("g")
+	.attr("class", "area")
+
+g.append("path")
+	.datum(data)
+	.attr("d", area);
+	
+var c = svg.append("g")
+    .attr("class", "color")
+	.attr("transform", "translate(0," + (height+2) + ")")
+
+c.append("rect")
+	.attr("width", width)
+	.attr("height", 16);
+	
+// axis
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (height+20) + ")")
+    .call(xAxis);
+
+
 </script>
 
 
