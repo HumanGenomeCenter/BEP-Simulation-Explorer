cca87790eb95329587152cd14244c11f35eeadb8
atusiniida <atusiniida@gmail.com>
2015-06-11 18:46:16 +0900

    add help

--- explorer/index.php
+++ explorer/index.php
@@ -52,7 +52,7 @@ include("../header.php");
 			<ul class="nav nav-tabs center" role="tablist">
 				<li role="presentation" class="active"><a href="#parameter" id="tab_parameter" aria-controls="home" role="tab" data-toggle="tab">Parameter-centered View</a></li>
 				<li role="presentation"><a href="#statistic" id="tab_statistic" aria-controls="profile" role="tab" data-toggle="tab">Statistic-centered View</a></li>
-				<li role="presentation"><a href="#help" id="tab_statistic" aria-controls="profile" role="tab" data-toggle="tab">Statistic-centered View</a></li>
+				<li role="presentation"><a href="#help" id="tab_statistic" aria-controls="profile" role="tab" data-toggle="tab">Help</a></li>
 			</ul>
 		</div>
 	</div>
@@ -73,11 +73,11 @@ include("../header.php");
 	<div role="tabpanel" class="tab-pane" id="help">
 		<p class="content2">
 		   Here, simulation results are interactively explored.  simulations were performed for different combinations of 4 parameter values: <i>s</i> (stem cell hierarchy), <i>r</i> (mutation rate), <i>d</i> (number of driver genes) and <i>f</i> (strength of driver mutations).
-		   From 20 independent simulation instances, avaraged 6 statistics were obtained: ε (population entropy), μ (founder mutation count),  ρ (average mutation count), λ (population fitness), τ (growth time)  and  θ (self-similarity).
+		   From 20 independent simulation instances, averaged 6 statistics were obtained: ε (population entropy), μ (founder mutation count),  ρ (average mutation count), λ (population fitness), τ (growth time)  and  θ (self-similarity).
 		   In <a href="#parameter" id="tab_parameter" aria-controls="home" role="tab" data-toggle="tab">Parameter-centered View</a>, <i>d-f</i> heatmaps of the 6 statistics are displayed  in one page for selected <i>s</i> and <i>r</i> values. 
 		   In <a href="#statistic" id="tab_statistic" aria-controls="profile" role="tab" data-toggle="tab">Statistic-centered View</a>, <i>d-f</i> heatmaps of each statistics for all examined combinations of <i>s</i> and <i>r</i> values are displayed in one page. 
-		   Colors of violin plots at the right of each d-f heat map indicates scales of the heatmap. If you use absolute scale, the scales  of <i>d-f</i> heatmaps of each statistic are common among all examined combinations of <i>s</i> and <i>r</i> values. If you use reletive scale, the scale is adjusted for each  <i>d-f</i> heatmap.
-	       If you click a cell in <i>d-f</i> heatmaps, images of 5 simulation instances from the corresponding parameter setting will apear below <i>d-f</i> heatmaps. By clicking each images, you can get details of the simulation instance.
+		   Colors of violin plots at the right of each d-f heat map indicates scales of the heatmap. If you use absolute scale, the scales  of <i>d-f</i> heatmaps of each statistic are common among all examined combinations of <i>s</i> and <i>r</i> values. If you use relative scale, the scale is adjusted for each  <i>d-f</i> heatmap.
+	       If you click a cell in <i>d-f</i> heatmaps, images of 5 simulation instances from the corresponding parameter setting will appear below <i>d-f</i> heatmaps. By clicking each image, you can get details of the simulation instance.
 	 	</p>
 	</div>
 	
