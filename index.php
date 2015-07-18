427f64f0475bf4bdc863b7a8311c267640d12368
新井田 厚司 <niiyan@niida-atsutsukasa-no-MacBook-Air-2.local>
2015-07-18 11:20:27 +0900

    add paper link

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			Cancer arises from accumulation of genomic mutations and accompanying evolutionary selection for growth advantage. During the evolutionary process, an ancestor clone branches into multiple clones, yielding intratumor heterogeneity. Recently, multiregional sequencing studies have revealed high genomic intratu- mor heterogeneity for several types of solid tumors. However, the principles underlying such high heterogeneity remains unclear. Here, to explore these principles, we built a cellular automaton model, termed the BEP model, which can reproduce heterogeneous evolution of cancer in silico. Employing a supercomputer enabled us to search extensively for conditions generating intratumor heterogeneity by changing parameters. Our data suggests that the existence of multiple driver genes, high mutation rate and a stem cell hierarchy are critical for heterogeneous tumor evolution. Moreover, we found that intratumor heterogeneity may have a fractal pattern, which is formed by neutral evolution. This study yields novel insights into cancer evolution principles, which can facilitate our understanding of cancer evolvability and robustness. 
 			</p>
 			<p class="content1">
-		    Our manuscript is avairable from here.
+		    Our manuscript is avairable from <a href="http://biorxiv.org/content/early/2015/07/17/022806">here</a>.
 		    </p>
 		    <p class="content1">
 		    The simulation study was performed by <a href="http://www.hgc.jp/~niiyan/profile/index.php">Atsushi Niida</a> (aniida at ims.u-tokyo.ac.jp), using <a href="http://www.aics.riken.jp/en/k-computer/about/">K computer</a>. <br>
